e072a9e3b2d62407e0f08afc285313e70f982894
L33hy <lehidaniel99@gmail.com>
2024-06-07 13:30:33 +0100

    picture book created and instances generated.

---
+++ sandbox/Sandbox/PictureBook.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class PictureBook
+{
+    private string _illustrator = "";
+
+    public string GetPictureBook()
+    {
+        return _illustrator;
+    }
+
+    public void SetPictureBook(string illustrator)
+    {
+        _illustrator = illustrator;
+    }
+}
