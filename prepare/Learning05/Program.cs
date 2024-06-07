07b68b9deb79c913b74414c32ff191cdfee6b926
L33hy <lehidaniel99@gmail.com>
2024-06-07 13:25:15 +0100

    added a few features to the main program file.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Student student = new Student("Lehi", "8135608930");
+        string name = student.GetName();
+        string number = student.GetNumber();
+        Console.WriteLine(name);
+        Console.WriteLine(number);
     }
 }
